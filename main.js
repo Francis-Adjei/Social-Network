202b041b0bd1334fc93ede3b270b08772d37e135
Francis-Adjei <francis.adjei@amalitech.org>
2021-04-19 09:36:52 -0700

    Assignment 7

---
+++ main.js
@@ -0,0 +1,25 @@
+let attempt = 7; 
+
+
+function validate(){
+	let username = document.getElementById("username").value; 
+	let password = document.getElementById("password").value; 
+
+	if ( username == "Francis" && password == "Amalitech"){ 
+		alert ("Login successfully");
+		window.location = "/finished.html";
+		return false;
+	}
+	else{
+		attempt --; 
+		alert("You have left "+attempt+" attempt!");
+
+		
+		if( attempt == 0){
+			document.getElementById("username").disabled = true;
+			document.getElementById("password").disabled = true;
+			document.getElementById("submit").disabled = true;
+			return false;
+		}
+	}
+}
